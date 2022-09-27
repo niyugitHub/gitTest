5d882255f6e25b91759304be7266e1562551b0f2
fko2247017 <fko2247017@FIC-PCROOM3B-21>
2022-09-27 12:09:47 +0900

    プロジェクト ファイルを追加します。

---
+++ game.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Game
+{
+	// ウインドウモード設定
+	constexpr bool kWindouwMode = true;
+	// ウインドウ名
+	const char* const kTitleText = "ゲーム名";
+	// ウインドウサイズ
+	constexpr int kScreenWidth = 1280;
+	constexpr int kScreenHeight = 720;
+	// カラーモード
+	constexpr int kColorDepth = 32;
+}
